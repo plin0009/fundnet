eff32f9c5d987b0c542e8f47e7ab7fa1d15dde23
plin0009 <plin0009@gmail.com>
2020-05-08 11:18:15 -0400

    Tabs for organization dashboard

--- web/src/pages/OrgMe.js
+++ web/src/pages/OrgMe.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery, useMutation } from "@apollo/react-hooks";
 import {
   GET_ORG_ME,
@@ -14,12 +14,19 @@ const OrgMe = ({ history }) => {
   const [changeBulletin] = useMutation(CHANGE_BULLETIN);
   const [addBulletin] = useMutation(ADD_BULLETIN);
   const [removeBulletin, { error }] = useMutation(REMOVE_BULLETIN);
-  const { bulletins, name, handle, website } = (data && data.orgMe) || {
+
+  const [tab, setTab] = useState("BULLETINS");
+
+  const { bulletins, postings, name, handle, website } = (data &&
+    data.orgMe) || {
     bulletins: [],
+    postings: [],
     name: "",
     handle: "",
     website: "",
   };
+
+  console.log(data && data.orgMe);
   const addNewBulletin = async ({ title, description, website, filters }) => {
     await addBulletin({
       variables: {
@@ -37,6 +44,7 @@ const OrgMe = ({ history }) => {
     });
     history.go();
   };
+
   return (
     <main>
       <section className="section">
@@ -50,36 +58,54 @@ const OrgMe = ({ history }) => {
           </div>
           <div className="tabs is-boxed">
             <ul>
-              <li className="is-active">
-                <a>Manage bulletins</a>
+              <li className={tab === "BULLETINS" ? "is-active" : null}>
+                <a role="button" onClick={() => setTab("BULLETINS")}>
+                  Manage bulletins
+                </a>
               </li>
-              <li>
-                <a>Manage job postings</a>
+              <li className={tab === "POSTINGS" ? "is-active" : null}>
+                <a role="button" onClick={() => setTab("POSTINGS")}>
+                  Manage job postings
+                </a>
               </li>
-              <li>
-                <a>Edit account</a>
+              <li className={tab === "ACCOUNT" ? "is-active" : null}>
+                <a role="button" onClick={() => setTab("ACCOUNT")}>
+                  Edit account
+                </a>
               </li>
             </ul>
           </div>
-          <h2 className="title is-4">My bulletins</h2>
-          <Tiles>
-            {[
-              ...bulletins.map((bulletin) => (
-                <EditableBulletin
-                  before={bulletin}
-                  onSaveEdit={editBulletin}
-                  onRemove={() =>
-                    removeBulletin({
-                      variables: {
-                        id: bulletin._id,
-                      },
-                    })
-                  }
-                />
-              )),
-              <EditableBulletin isNew={true} onSaveEdit={addNewBulletin} />,
-            ]}
-          </Tiles>
+
+          {tab === "BULLETINS" ? (
+            <div>
+              <h2 className="title is-4">My bulletins</h2>
+              <Tiles>
+                {[
+                  ...bulletins.map((bulletin) => (
+                    <EditableBulletin
+                      before={bulletin}
+                      onSaveEdit={editBulletin}
+                      onRemove={() =>
+                        removeBulletin({
+                          variables: {
+                            id: bulletin._id,
+                          },
+                        })
+                      }
+                    />
+                  )),
+                  <EditableBulletin isNew={true} onSaveEdit={addNewBulletin} />,
+                ]}
+              </Tiles>
+            </div>
+          ) : tab === "POSTINGS" ? (
+            <div>
+              <h2 className="title is-4">My postings</h2>
+              <Tiles>{[...postings.map((posting) => <></>), <></>]}</Tiles>
+            </div>
+          ) : (
+            <div></div>
+          )}
         </div>
       </section>
     </main>
